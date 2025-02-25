2887c0f82261fc9b2010970aeff07d6e510a63c6
Jules Fowler <juliafowlerm@gmail.com>
2025-02-25 12:16:09 -0800

    Still working through this demo.

--- Revised_Focus_Diversity.py
+++ Revised_Focus_Diversity.py
@@ -8,28 +8,47 @@ from image_sharpening import FocusDiversePhaseRetrieval, ft_rev, mft_rev, Instru
 from processing import phase_unwrap_2d
 #pep8 style
 
-    seal_params = {'image_dx': 2.0071, # 
-               'efl': focal_length*1e3, # SEAL effective focal length, mm
-               'wavelength': 0.65, # SEAL center wavelength, microns
-                'pupil_size': pupil_size*1e3, # Keck entrance pupil diameter
-                    }
-    # Configure instrument
-    conf = InstrumentConfiguration(seal_params)
 
-def calculate_phase_retrieval_accuracy():
+def convert_psf_estimate_to_phase(psf_estimate, seal_parameters):
+    pass 
+
+def check_phase_estimate():
+    pass
+
+def make_cost_function_plots():
+    pass
+
+def calculate_phase_retrieval_accuracy(system_truth, psf_estimate, cost_functions, verbose=False):
+
+    phase_estimate = convert_psf_estimate_to_phase(psf_estimate, seal_parameters)
+    
+    phase_estimate_metrics = check_phase_estimate(system_truth, phase_estimate)
+
+    if verbose:
+        make_cost_function_plots(cost_functions)
+
+    return phase_estimate_metrics
 
 
 def simulate_focused_image():
-
+    pass
 
 def simulate_defocused_image():
+    pass 
 
+def run_phase_retrieval(system_truth, defocus_dictionary, seal_parameters):
+    
+    distance_list = list(defocus_dictionary.keys())
+    psf_list = [system_truth] + [defocus_dictionary[key] for key in distance_list]
+    dx_list = [seal_parameters['image_dx'] for key in distance_list]
 
-def run_phase_retrieval():
-    mp= FocusDiversePhaseRetrieval(psf_list,650e-3, dx_list, distance_list)
+    mp= FocusDiversePhaseRetrieval(psf_list, seal_parameters['wavelength'], dx_list, distance_list)
     for i in range(200):
-        psf00 = mp.step() 
-
+        psf_product = mp.step() 
+    
+    psf_estimate = np.angle(psf_product)
+    
+    return psf_estimate, mp.cost_functions 
 
 
 def focus_diverse_phase_retrieval(system_truth, phase_diverse_inputs):
@@ -38,23 +57,25 @@ def focus_diverse_phase_retrieval(system_truth, phase_diverse_inputs):
     for defocus_distance in phase_diverse_inputs:
         defocus_dictionary[defocus_distance] = simulate_defocused_image()
 
-
-    run_phase_retrieval(system_truth, defocus_dictionary)
-
+    psf_estimate, cost_functions = run_phase_retrieval(system_truth, defocus_dictionary, seal_parameters)
+    
+    return psf_estimate, cost_functions
 
 
 
 def simulate_phase_diversity_grid(phase_diversity_grid):
     system_truth = simulate_focused_image() 
     for phase_diverse_inputs in phase_diversity_grid:
-        focus_diverse_phase_retrieval(system_truth, phase_diverse_inputs)
-        calculate_phase_retrieval_accuracy()
+        
+        psf_estimate, cost_functions =
+        focus_diverse_phase_retrieval(system_truth, phase_diverse_inputs, seal_parameters)
+        calculate_phase_retrieval_accuracy(sytem_truth, psf_estimate, cost_functions, seal_parameters)
 
 
 
 def plot_phase_diversity_heat_map()
 
-def main():
+def main(seal_parameters):
     #Necessary to get Results for a Plot, using a predescribed unit
     simulate_phase_diversity_grid()
     #Last function is to plot, i.e 'plot phase diversity heat map'
@@ -63,8 +84,14 @@ def main():
 
 if __name__ == "__main__": 
     #Define Variables
+    seal_parameters = {'image_dx': 2.0071, # 
+               'efl': focal_length*1e3, # SEAL effective focal length, mm
+               'wavelength': 0.65, # SEAL center wavelength, microns
+                'pupil_size': pupil_size*1e3, # Keck entrance pupil diameter
+                    }
+
     #function call(either 1 or specifically named call)
-    main()
+    main(seal_parameters)
 
 
 
